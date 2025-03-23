88ec66e43d9a8f2d0c7c1bba404b476dcaabaa4a
Veronica Cattivera <verocattivera@Veronicas-MacBook-Pro.local>
2025-03-22 21:38:38 -0400

    Added methods to create and modify a pet store, also created an error handler

---
+++ src/main/java/pet/store/service/PetStoreService.java
@@ -0,0 +1,52 @@
+package pet.store.service;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pet.store.controller.model.PetStoreData;
+import pet.store.dao.PetStoreDao;
+import pet.store.entity.PetStore;
+
+@Service
+public class PetStoreService {
+	
+	@Autowired
+	private PetStoreDao petStoreDao;
+
+	public PetStoreData savePetStore(PetStoreData petStoreData) {
+		Long  petStoreId = petStoreData.getPetStoreId();
+		PetStore petStore = findOrCreatePetStore(petStoreId);
+		
+		copyPetStoreFields(petStore, petStoreData);
+		return new PetStoreData(petStoreDao.save(petStore));
+	}
+
+	private void copyPetStoreFields(PetStore petStore, PetStoreData petStoreData) {
+		petStore.setPetStoreAddress(petStoreData.getPetStoreAddress());
+		petStore.setPetStoreCity(petStoreData.getPetStoreCity());
+		petStore.setPetStoreId(petStoreData.getPetStoreId());
+		petStore.setPetStoreName(petStoreData.getPetStoreName());
+		petStore.setPetStorePhone(petStoreData.getPetStorePhone());
+		petStore.setPetStoreState(petStoreData.getPetStoreState());
+		petStore.setPetStoreZip(petStoreData.getPetStoreZip());
+	}
+
+	private PetStore findOrCreatePetStore(Long petStoreId) {
+		if(Objects.isNull(petStoreId)) {
+			return new PetStore();
+		}
+		else {
+			return findPetStoreById(petStoreId);
+		}
+	}
+
+	private PetStore findPetStoreById(Long petStoreId) {
+		// TODO Auto-generated method stub
+		return petStoreDao.findById(petStoreId).orElseThrow(() -> new NoSuchElementException(
+				"Pet store with ID=" + petStoreId + " was not found."));
+	}
+	
+}
